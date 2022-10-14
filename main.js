da62891fad14913a1d2e51f43fda961f70f8a7ca
(SSB-61B9) Vuong.Nguyen <vuong.nguyen@ssbshoes.com>
2022-10-14 14:07:45 +0700

    done!

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
 //Select element function
+const selectElement = (element) => {
+  return document.querySelector(element);
+}
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', () => body.classList.toggle('open'));
+
+// scroll reveal
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+  origin: 'left',
+  duration: 1000,
+  distance: '25rem',
+  delay: 100
+});
+
+sr.reveal('.animate-right', {
+  origin: 'right',
+  duration: 1000,
+  distance: '25rem',
+  delay: 200
+});
+
+sr.reveal('.animate-top', {
+  origin: 'top',
+  duration: 1000,
+  distance: '25rem',
+  delay: 200
+});
+
+sr.reveal('.animate-bottom', {
+  origin: 'bottom',
+  duration: 1000,
+  distance: '25rem',
+  delay: 200
+});
